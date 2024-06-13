2a43340cfb01a182eaa2ad74b7fd90a0f9abddc2
jcsgithub <jestonisalonga@gmail.com>
2024-06-14 02:05:38 +0800

    feat(api): clerk setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://strong-jaybird-89.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+}
+
+export default authConfig
